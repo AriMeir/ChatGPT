5aae85cc8ab5a5ef6b31ab69b433891e9e401274
AriMeir <AriMeir95@gmail.com>
2024-12-31 13:53:14 +0200

    continue with the UI

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,27 @@ function App() {
         </div>
       </aside>
       <section className='chatbox'>
+        <div className='chat-log'>
+          <div className='chat-message'>
+            <div className='avatar'>
+             
+            </div>
+            <div className='message'>
+              Hello world
+            </div>
+          </div>
 
+        </div>
+        <div className='chat-input-holder'>
+          <textarea rows={1}
+            className="chat-input-textarea">
+          </textarea>
+          
+        </div>
+
+        <p className='bottom-text'>chatGPT can make mistakes. check important info.</p>
       </section>
+      
       
     </div>
   );
